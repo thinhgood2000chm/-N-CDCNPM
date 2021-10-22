3579f1dc3cd37d6a0e60ebc99c4b347812afda51
XuanThinh <thinhgood9@gmail.com>
2021-10-22 20:38:44 +0700

    [add]: finish treeview, list view not complete

--- fileExplore/fileExplore/Form1.Designer.cs
+++ fileExplore/fileExplore/Form1.Designer.cs
@@ -29,21 +29,73 @@ namespace fileExplore
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.treeViewEx = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // treeViewEx
             // 
-            this.treeViewEx.Location = new System.Drawing.Point(12, 12);
+            this.treeViewEx.ImageIndex = 0;
+            this.treeViewEx.ImageList = this.imageList1;
+            this.treeViewEx.Location = new System.Drawing.Point(0, 60);
             this.treeViewEx.Name = "treeViewEx";
-            this.treeViewEx.Size = new System.Drawing.Size(256, 426);
+            this.treeViewEx.SelectedImageIndex = 2;
+            this.treeViewEx.Size = new System.Drawing.Size(182, 391);
             this.treeViewEx.TabIndex = 0;
+            this.treeViewEx.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeViewEx_AfterSelect);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "directory.png");
+            this.imageList1.Images.SetKeyName(1, "file.png");
+            this.imageList1.Images.SetKeyName(2, "Imageres_dll_027.ico");
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(187, 60);
+            this.listView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(602, 390);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 350;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Type";
+            this.columnHeader2.Width = 170;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Last Modified";
+            this.columnHeader3.Width = 200;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.treeViewEx);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -55,6 +107,11 @@ namespace fileExplore
         #endregion
 
         private System.Windows.Forms.TreeView treeViewEx;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
     }
 }
 
