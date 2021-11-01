5bc8b057d657462584e79ed0a628fa241b0fa163
XuanThinh <thinhgood9@gmail.com>
2021-11-01 23:29:32 +0700

    add description

--- fileExplore/fileExplore/Form1.cs
+++ fileExplore/fileExplore/Form1.cs
@@ -62,10 +62,10 @@ namespace fileExplore
         }
 
         private void PopulateTreeView()
-        {
+        {// khởi tạo root gốc trong tree node 
             TreeNode rootNode;
-            var ListDriverInfor = DriveInfo.GetDrives();
-            foreach (DriveInfo drive in ListDriverInfor)
+            var ListDriverInfor = DriveInfo.GetDrives();// lây tất cả các ổ đĩa ( các ổ đia trong máy, ko bao gồm các file trong ổ đĩa)
+            foreach (DriveInfo drive in ListDriverInfor) // bắt đầu tìm kiếm trong các ổ đĩa để lấy ra các folder và các file 
             {
                 string path = drive.Name.ToString();
                 DirectoryInfo info = new DirectoryInfo(path);
@@ -73,12 +73,12 @@ namespace fileExplore
                 if (info.Exists)
                 {
 
-                    rootNode = new TreeNode(info.Name);
-                    rootNode.ImageIndex = 2;
+                    rootNode = new TreeNode(info.Name);// nếu như có tồn tại thư mục con năm trong path ( path là đường dẫn vd khi bắt đầu với ổ c path sẽ là C) 
+                    rootNode.ImageIndex = 2;// gắn image cho root node ( đây là image dành cho ổ đĩa c d e ... , các folder được gắn mặc định ) 
                     rootNode.Tag = info;
-                    GetDirectories(info.GetDirectories(), rootNode);
+                    GetDirectories(info.GetDirectories(), rootNode);// tìm kiếm các folder bên trong ổ đĩa
              
-                    treeViewEx.Nodes.Add(rootNode);
+                    treeViewEx.Nodes.Add(rootNode);// thêm root node vào tree view để tạo ra nhánh của 1 ổ đĩa 
                 }
             }
             /*        DirectoryInfo info = new DirectoryInfo(@"E:\filehoctap");
@@ -96,7 +96,7 @@ namespace fileExplore
         {
             TreeNode aNode;
             DirectoryInfo[] subSubDirs;
-            foreach (DirectoryInfo subDir in subDirs)
+            foreach (DirectoryInfo subDir in subDirs) // bắt đầu tìm kiếm trong từng ổ đĩa 
             {
                 aNode = new TreeNode(subDir.Name, 0, 0);
                 aNode.Tag = subDir;
@@ -107,9 +107,9 @@ namespace fileExplore
                     subSubDirs = subDir.GetDirectories(); 
                     if (subSubDirs.Length != 0)
                     {
-                        GetDirectories(subSubDirs, aNode);
+                        GetDirectories(subSubDirs, aNode);// cái này gọi là đệ quy sau khi tìm xong 1 folder sẽ tiếp tục tìm kiếm lại trong folder con của folder đó xem có còn file hay folder nào nữa ko 
                     }
-                    nodeToAddTo.Nodes.Add(aNode);
+                    nodeToAddTo.Nodes.Add(aNode);// adđ folder vào ổ đĩa 
                 }
                 catch (UnauthorizedAccessException)
                 {
@@ -121,7 +121,7 @@ namespace fileExplore
                 }
             }
         }
-
+        // thiết lập tree view mỗi khi bấm vào thì list view sẽ chuyển theo ứng vs tree view
         private void treeViewEx_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
         {
             TreeNode newSelected = e.Node;
